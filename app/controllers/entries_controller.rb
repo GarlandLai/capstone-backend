99c95722bdd795e3627096ff6eeaece698480906
Student <example@example.com>
2019-09-30 09:33:56 -0700

    add entries controller methods for CRUD

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,49 @@
+class EntriesController < AppicationController
+    
+    def index 
+        @entries = Entry.all 
+         json_response(@entries)
+    end
+
+    def show
+        @entry = Entry.find(params[:id])
+        json_response(@entry)
+    end
+
+    def create
+        @entry = Entry.Create!(entry_params)
+        json_response(@entry, :created)
+    end
+
+    def update
+        @entry = Entry.find(params[:id])
+        @entry.update!(entry_params)
+        if @entry.update!(entry_params)
+            render status: 200, json: {
+                message: "The entry has been successfully updated!"
+            }
+        end
+    end
+    
+    def destroy
+        @entry = Entry.find(params[:id])
+        @entry.destroy
+        if @entry.destroy!
+            render status: 200, json: {
+                message: "The entry has been deleted."
+            }   
+         end
+    end
+
+    private
+
+    def json_response(object, status = :ok)
+        render json: object, status: status
+        
+    end
+
+    def entry_params
+       params.permit(:prompt1, :prompt2, :prompt3, :content) 
+    end
+    
+end
--- db/migrate/20190930160948_create_entries.rb
+++ db/migrate/20190930160948_create_entries.rb
@@ -1,6 +1,11 @@
 class CreateEntries < ActiveRecord::Migration[5.2]
   def change
     create_table :entries do |t|
+      t.column(:prompt1, :string)
+      t.column(:prompt2, :string)
+      t.column(:prompt3, :string)
+      t.column(:content, :text)
+      t.timestamps()
     end
   end
 end
